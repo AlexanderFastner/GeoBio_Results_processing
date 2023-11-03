081d33e52a356a55b7d1667d0a565b122302dfcb
Alexander  Fastner <afastne@wla128.palmuc.intranet>
2023-11-03 13:06:32 +0100

    add options to script 1, add some stuff to script 2

--- scripts/full_tables.py
+++ scripts/full_tables.py
@@ -18,7 +18,6 @@ import subprocess
 pattern = '.TransPi.bus4.tsv'
 pattern2 = '.Trinity.bus4.tsv'
 patterns = [pattern, pattern2]
-search_dir = '/netvolumes/srva229/molpal/hpc_exchange/Alex/GeoBio_Results_processing/full_tables'
 fasta_dir = '/netvolumes/srva229/molpal/hpc_exchange/Alex/GeoBio_Results_processing/Cnidaria/'
 output_dir = '/netvolumes/srva229/molpal/hpc_exchange/Alex/GeoBio_Results_processing/full_tables_output/'
 #-------------------------------------------------------------------------------------------------------------------------------
@@ -37,84 +36,86 @@ for d in os.listdir(fasta_dir):
         os.mkdir(path+'/evigene')
 
 #-------------------------------------------------------------------------------------------------------------------------------
-i=0
-while i < len(patterns):
-    for f in os.listdir(search_dir):
-        if patterns[i] in f:
-            print(f)
-            file_basename = f.split('.')[0][11:]
-            #print(file_basename)
-            
+
+for species in os.listdir(fasta_dir):
+    print(species)
+    i=0
+    while i < len(patterns):
+        print(f'{fasta_dir}{species}/busco4/full_table_{species}{patterns[i]}')
+        with open(f'{fasta_dir}{species}/busco4/full_table_{species}{patterns[i]}','r') as table:
             #make a list of things to be grepped for
-            with open(f'./grep_search_{file_basename}{patterns[i]}', 'w') as grep_file:
-                with open(search_dir + '/' + f, 'r') as file:
-                    sequence = 'Missing!'
-                    busco_id = ''
-                    for num, line in enumerate(file):
-                        if num < 3:
-                            continue
-                        line = line.strip().split('\t')
-                        #print(line)
-                        busco_id = line[0]
-                        #print(busco_id)
-                        if(len(line) > 2):
-                            sequence = line[2]
-                            #print(sequence)
-                        grep_file.write(f'{busco_id}\t{sequence}\n')
-                        
+            with open(f'./grep_search_{species}{patterns[i]}', 'w') as grep_file:
+                sequence = 'Missing!'
+                busco_id = ''
+                for num, line in enumerate(table):
+                    if num < 3:
+                        continue
+                    line = line.strip().split('\t')
+                    #print(line)
+                    busco_id = line[0]
+                    #print(busco_id)
+                    if(len(line) > 2):
+                        sequence = line[2]
+                        #print(sequence)
+                    grep_file.write(f'{busco_id}\t{sequence}\n')
+            print(f'finished making grep file {species}{patterns[i]}')
 
-            #TransPi
-            if i == 0:
-                #print(f'{fasta_dir}{file_basename}/evigene/{file_basename}.combined.okay.fa')
-                
-                transcriptome_to_fix = f'{fasta_dir}{file_basename}/evigene/{file_basename}.combined.okay.fa'
-                
-                awk = f'awk \'{{ if($1 ~ />/){{ print $0 }} else{{ gsub(/{chr(92)}n$/,\"\"); printf "%s", $0 }}}}\' {transcriptome_to_fix} | sed \'s/>/{chr(92)}n>/g\' > {transcriptome_to_fix}_single_line.tmp'
-                
-                subprocess.run([awk], shell=True, capture_output=True, text=True)
-                
-                with open(f'./grep_search_{file_basename}.TransPi.bus4.tsv', 'r') as grep:
-                    for line in grep:
-                        split_line = line.strip('\n').split('\t')
-                        seq = split_line[1]
-                        busco = split_line[0]
-                        if 'Missing!' not in seq:
-                            grep_cmd = f'grep {seq} -A 1 --no-group-separator {transcriptome_to_fix}_single_line.tmp'
-                            #print(grep_cmd)
-                            result = subprocess.run([grep_cmd], shell=True, capture_output=True, text=True).stdout.split('\n')
-                            header = result[0]
-                            sequence = result[1]
-                            with open(output_dir + file_basename + '/evigene/' + busco + '.fa', 'a') as busco_new:
-                                busco_new.write(f'{header}\n')
-                                busco_new.write(f'{sequence}\n')
-                                    
-                                
-                #remove tmp file
-                remove_tmp = f'rm {transcriptome_to_fix}_single_line.tmp'
-                subprocess.run([remove_tmp], shell=True, capture_output=True, text=True)
-                print(f'finished Transpi {file_basename}')
 
-                
-            #Trinity
-            else:
-                with open(f'./grep_search_{file_basename}.Trinity.bus4.tsv', 'r') as grep:
-                    for line in grep:
-                        split_line = line.strip('\n').split('\t')
-                        seq = split_line[1]
-                        busco = split_line[0]
-                        if 'Missing!' not in seq:
-                            grep_cmd = f'grep {seq} -A 1 --no-group-separator {fasta_dir}{file_basename}/assemblies/{file_basename}.Trinity.fa'
-                            #print(grep_cmd)
-                            result = subprocess.run([grep_cmd], shell=True, capture_output=True, text=True).stdout.split('\n')
-                            header = result[0]
-                            sequence = result[1]
-                            with open(output_dir + file_basename + '/assemblies/' + busco + '.fa', 'a') as busco_new:
-                                busco_new.write(f'{header}\n')
-                                busco_new.write(f'{sequence}\n')
-                print(f'finished Trinity {file_basename}')
+        #TransPi
+        if i == 0:
+            #print(f'{fasta_dir}{species}/evigene/{species}.combined.okay.fa')
 
+            transcriptome_to_fix = f'{fasta_dir}{species}/evigene/{species}.combined.okay.fa'
+
+            awk = f'awk \'{{ if($1 ~ />/){{ print $0 }} else{{ gsub(/{chr(92)}n$/,\"\"); printf "%s", $0 }}}}\' {transcriptome_to_fix} | sed \'s/>/{chr(92)}n>/g\' > {transcriptome_to_fix}_single_line.tmp'
+
+            subprocess.run([awk], shell=True, capture_output=True, text=True)
+
+            with open(f'./grep_search_{species}.TransPi.bus4.tsv', 'r') as grep:
+                for line in grep:
+                    split_line = line.strip('\n').split('\t')
+                    seq = split_line[1]
+                    busco = split_line[0]
+                    if 'Missing!' not in seq:
+                        grep_cmd = f'grep {seq} -A 1 --no-group-separator {transcriptome_to_fix}_single_line.tmp'
+                        #print(grep_cmd)
+                        result = subprocess.run([grep_cmd], shell=True, capture_output=True, text=True).stdout.split('\n')
+                        header = result[0]
+                        sequence = result[1]
+                        with open(output_dir + species + '/evigene/' + busco + '.fa', 'a') as busco_new:
+                            busco_new.write(f'{header}\n')
+                            busco_new.write(f'{sequence}\n')
+
+            #remove tmp file
+            remove_tmp = f'rm {transcriptome_to_fix}_single_line.tmp'
+            subprocess.run([remove_tmp], shell=True, capture_output=True, text=True)
+            print(f'finished Transpi {species}')
+
+
+        #Trinity
+        else:
+            with open(f'./grep_search_{species}.Trinity.bus4.tsv', 'r') as grep:
+                for line in grep:
+                    split_line = line.strip('\n').split('\t')
+                    seq = split_line[1]
+                    busco = split_line[0]
+                    if 'Missing!' not in seq:
+                        grep_cmd = f'grep {seq} -A 1 --no-group-separator {fasta_dir}{species}/assemblies/{species}.Trinity.fa'
+                        #print(grep_cmd)
+                        result = subprocess.run([grep_cmd], shell=True, capture_output=True, text=True).stdout.split('\n')
+                        header = result[0]
+                        sequence = result[1]
+                        with open(output_dir + species + '/assemblies/' + busco + '.fa', 'a') as busco_new:
+                            busco_new.write(f'{header}\n')
+                            busco_new.write(f'{sequence}\n')
+            print(f'finished Trinity {species}')
                     
         print()
-    i+=1
+        #TODO rm all ./grep_search files
+        print(f'test remove: rm ./grep_search_{species}{patterns[i]}')
+        rm_cmd = f'test remove: rm ./grep_search_{species}{patterns[i]}'
+        subprocess.run([rm_cmd], shell=True, capture_output=True, text=True)
+        
+        i+=1
 #-------------------------------------------------------------------------------------------------------------------------------    
     
--- scripts/short_summaries.py
+++ scripts/short_summaries.py
@@ -2,42 +2,122 @@
 
 #This is a python script to go through all files in a dir and summarize the content in one large table
 #Do for .Transpi.bus4.txt and for .Trinity.bus4.txt
-#---------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------
 import os
-#---------------------------------------------------------------------------------------------------------------------------------
+import typer
+from typing_extensions import Annotated
+#-------------------------------------------------------------------------------------------------------------------------------
+app = typer.Typer()
 
-pattern = '.TransPi.bus4.txt'
-pattern2 = '.Trinity.bus4.txt'
-patterns = [pattern, pattern2]
-outputs = ['TransPi.tsv','Trinity.tsv']
-search_dir = '/netvolumes/srva229/molpal/hpc_exchange/Alex/GeoBio_Results_processing/short_summaries'
+@app.command()
+def main(file_list: Annotated[str, typer.Argument()] = ''):
+    """
+    Python script to go through all files in a dir and summarize the content in one large table
+    Default is ../short_summaries
+    """    
 
-for j, entry in enumerate(outputs):
-    with open (entry, 'a') as out:
-        out.write(f'species_name\tC\tS\tD\tF\tM\tTotal\n')
-        for f in os.listdir(search_dir):
-            if patterns[j] in f:
-                with open(search_dir + '/' + f, 'r') as file:
-                    species_name = ''
-                    c = s = d = f = m = total = 0
-                    for i, line in enumerate(file):
-                        #print(i)
-                        line = line.strip()
-                        #print(line)
-                        if i == 2:
-                            sub = line.split(' ')
-                            species_name = str(sub[-1:][0]).split('.')[0]
-                        if i == 8:
-                            c = line.split('\t')[0]
-                        if i == 9:
-                            s = line.split('\t')[0]
-                        if i == 10:
-                            d = line.split('\t')[0]
-                        if i == 11:
-                            f = line.split('\t')[0]
-                        if i == 12:
-                            m = line.split('\t')[0]
-                        if i == 13:
-                            total = line.split('\t')[0]
-                out.write(f'{species_name}\t{c}\t{s}\t{d}\t{f}\t{m}\t{total}\n')
-    out.close()
+    pattern = '.TransPi.bus4.txt'
+    pattern2 = '.Trinity.bus4.txt'
+    patterns = [pattern, pattern2]
+    outputs = ['TransPi.tsv','Trinity.tsv']
+    search_dir = '../short_summaries'
+    out_dir = '../short_summaries_results'
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
+        
+#-------------------------------------------------------------------------------------------------------------------------------
+
+    #TODO replace search dir with inputted
+
+    def make_file_list(given_list):
+        for j, entry in enumerate(outputs):
+            with open (f'{out_dir}/{entry}', 'a') as out:
+                out.write(f'species_name\tC\tS\tD\tF\tM\tTotal\n')
+                for f in os.listdir(search_dir):
+                    base_name = f.split('.')[3].strip()
+                    #if on the list
+                    if (len(given_list) > 1):
+#                         print(given_list)
+#                         print()
+#                         print(base_name)
+                        if base_name in given_list:
+                            if patterns[j] in f:
+                                print(base_name)
+                                with open(search_dir + '/' + f, 'r') as file:
+                                    species_name = ''
+                                    c = s = d = f = m = total = 0
+                                    for i, line in enumerate(file):
+                                        #print(i)
+                                        line = line.strip()
+                                        #print(line)
+                                        if i == 2:
+                                            sub = line.split(' ')
+                                            species_name = str(sub[-1:][0]).split('.')[0]
+                                        if i == 8:
+                                            c = line.split('\t')[0]
+                                        if i == 9:
+                                            s = line.split('\t')[0]
+                                        if i == 10:
+                                            d = line.split('\t')[0]
+                                        if i == 11:
+                                            f = line.split('\t')[0]
+                                        if i == 12:
+                                            m = line.split('\t')[0]
+                                        if i == 13:
+                                            total = line.split('\t')[0]
+                                out.write(f'{species_name}\t{c}\t{s}\t{d}\t{f}\t{m}\t{total}\n')
+                    else:
+                        print('list not long enough')
+            out.close()
+
+    def make_file():
+        for j, entry in enumerate(outputs):
+            with open (f'{out_dir}/{entry}', 'a') as out:
+                out.write(f'species_name\tC\tS\tD\tF\tM\tTotal\n')
+                for f in os.listdir(search_dir):
+                    if patterns[j] in f:
+                        with open(search_dir + '/' + f, 'r') as file:
+                            species_name = ''
+                            c = s = d = f = m = total = 0
+                            for i, line in enumerate(file):
+                                #print(i)
+                                line = line.strip()
+                                #print(line)
+                                if i == 2:
+                                    sub = line.split(' ')
+                                    species_name = str(sub[-1:][0]).split('.')[0]
+                                if i == 8:
+                                    c = line.split('\t')[0]
+                                if i == 9:
+                                    s = line.split('\t')[0]
+                                if i == 10:
+                                    d = line.split('\t')[0]
+                                if i == 11:
+                                    f = line.split('\t')[0]
+                                if i == 12:
+                                    m = line.split('\t')[0]
+                                if i == 13:
+                                    total = line.split('\t')[0]
+                        out.write(f'{species_name}\t{c}\t{s}\t{d}\t{f}\t{m}\t{total}\n')
+            out.close()
+            
+#------------------------------------------------------------------------------------------------------------------------------- 
+
+    #if len(file_list) is not None:
+    if len(file_list) > 1:
+        species = []
+        with open(file_list, 'r') as files:
+            for line in files:
+                print(f'line : {line}')
+                species.append(line.strip())
+        #go through all files in species
+        make_file_list(species)
+
+    else:
+        #do for all in ../short_summaries
+        make_file()
+    
+#-------------------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app()
+#-------------------------------------------------------------------------------------------------------------------------------
